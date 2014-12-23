ff35987a2a921e0c7d3e6a4eb4969725e59914ab
George Taveras <gtaveras@xogrp.com>
2014-12-23 10:20:33 -0500

    transitioned CLI spec towards bdd/integration specs

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,4 +1,3 @@
-require 'spec_helper'
 
 RSpec::Matchers.define :exit_with_code do |expected|
   supports_block_expectations
@@ -17,14 +16,28 @@ RSpec::Matchers.define :exit_with_code do |expected|
 end
 
 describe Piv::CLI do
-  let(:global_dir) { File.join(__dir__, 'fixtures', 'piv_test') }
+  def allow_exit!
+    @allow_exit = true
+  end
 
-  let(:api_url) do
-    "https://www.pivotaltracker.com/services/v5/"
+  def allow_exit?
+    @allow_exit
+  end
+
+  let(:global_dir) { File.join(__dir__, 'fixtures', 'piv_test') }
+  let(:api_url) { "https://www.pivotaltracker.com/services/v5/" }
+
+  let(:connection) do
+    Faraday.new(api_url) do |conn|
+      conn.request :json
+      conn.response :json
+      conn.adapter Faraday.default_adapter
+    end
   end
 
   before do
     Piv::Application.global_dir = global_dir
+    Piv::Application.connection = connection
   end
 
   after do
@@ -33,116 +46,91 @@ describe Piv::CLI do
     end
   end
 
+
   let(:run_command) do
-    if defined? stub_exit
-      begin
+    silence_stream($stdout) do
+      if allow_exit?
+        begin
+          Piv::CLI.start(argv)
+        rescue SystemExit
+        end
+      else
         Piv::CLI.start(argv)
-      rescue SystemExit
       end
-    else
-      Piv::CLI.start(argv)
     end
   end
 
   describe 'login' do
     let(:argv) { %w( login ) }
+    let(:basic_auth_url) { 'https://user:password@www.pivotaltracker.com/services/v5/me' }
 
-    let(:application) do
-      double(:application,
-        :assure_globally_installed => nil,
-        :session_in_progress? => session_in_progress?,
-        :user_wants_to_preserve_session? => user_wants_to_preserve_session?,
-        )
-    end
-
-    before do
-      allow(Piv::Session).to receive(:first_or_create) { session }
-
-      allow(Piv::Application).to receive(:for).with(:login).and_yield(application)
-    end
-
-    context "always" do
-      let(:session_in_progress?) { true }
-      let(:user_wants_to_preserve_session?) { true }
-      let(:stub_exit) { true }
-
-      it "assures piv is globally installed" do
-        expect(application).to receive(:assure_globally_installed)
-        run_command
-      end
-    end
-
-    context 'when session is in progress and user wants to preserve session' do
-      let(:session_in_progress?) { true }
-      let(:user_wants_to_preserve_session?) { true }
-
-      it 'exits with a code of 0' do
-        expect { run_command }.to exit_with_code(0)
-      end
-    end
-
-    context 'when there is no session in progress' do
-      let(:session_in_progress?) { false }
-      let(:user_wants_to_preserve_session?) { false }
+    describe "behavior" do
 
       before do
-        allow(application).to receive(:ask_for_credentials) { ['user', 'password'] }
-        allow(application).to receive(:client) { client }
-
-        allow(Piv::Session).to receive(:first_or_create) { session }
+        Piv::Application.for(:login).assure_globally_installed
+        stub_request(:get, basic_auth_url).to_return(
+          :status => 200,
+          :body => {
+            :api_token => 'abc123',
+            :name => 'george'}.to_json)
       end
 
-      let(:session) { double(:session, :current= => true, :save => true) }
-      let(:client) { double(:client, :login => response) }
+      context 'when session is in progress and user wants to preserve session' do
+        before do
+          Piv::Session.create(:token => '123', :current => true)
+          allow($stdin).to receive(:gets) { 'n' }
+        end
 
-      let(:response) { double(:response, :status => nil, :body => {}) }
+        it "asks user if he wants to preserve session" do
+          allow_exit!
+          expect { run_command }.to output(/start a new session\?.*\[yn\].*$/i).to_stdout
+        end
 
-      describe do
-        let(:stub_exit) { true }
-
-        it 'asks the user for credentials' do
-          expect(application).to receive(:ask_for_credentials)
-          run_command
+        it 'exits with a code of 0' do
+          expect { run_command }.to exit_with_code(0)
         end
       end
 
-      describe "<request>" do
+      context 'when there is no session in progress' do
+        before do
+          Piv::Session.destroy_all
+          allow($stdin).to receive(:gets).and_return('user', 'password')
+        end
 
-        context "always" do
+        it 'asks the user for credentials' do
+          allow_exit!
+          expect { run_command }.to output(/User: .*Password: .*/).to_stdout
+          run_command
+        end
 
-          let(:stub_exit) { true }
+        describe "<request>" do
+
+          before do
+            allow($stdin).to receive(:gets).and_return('user', 'password')
+          end
+
+          let(:request) do
+            a_request(:get, basic_auth_url)
+          end
 
           it "makes a login request with user credentials" do
-            expect(client).to receive(:login)
-              .with(:user => 'user', :password => 'password')
-
+            allow_exit!
             run_command
+            expect(request).to have_been_made
           end
         end
 
         describe "<response>" do
-          context "when there is a server error" do
-            before do
-              allow(client).to receive(:login) { raise Piv::Client::NetworkError, 'somsom' }
-            end
+          before do
+            stub_request(:get, basic_auth_url).to_return(
+              :status => status,
+              :body => body.to_json)
 
-            it "exits with a status of 1" do
-              expect { run_command }.to exit_with_code(1)
-            end
-
-            describe do
-              let(:stub_exit) { true }
-
-              it "prints the error msg" do
-                expect { run_command }.to output(/somsom/).to_stderr
-              end
-            end
+            allow($stdin).to receive(:gets).and_return('user', 'password')
           end
 
           context "when response status is 200" do
-            let(:response) do
-              double(:response, :status => 200, :body => body)
-            end
+            let(:status) { 200 }
 
             let(:body) do
               {
@@ -155,19 +143,15 @@ describe Piv::CLI do
               expect { run_command }.to exit_with_code(0)
             end
 
-            describe do
-              let(:stub_exit) { true }
-
-              it "prints a welcoming message" do
-                expect { run_command }.to output(/You have been authenticated./).to_stdout
-              end
+            it "prints a welcoming message" do
+              allow_exit!
+              expect { run_command }.to output(/You have been authenticated./).to_stdout
             end
-
           end
 
           context "when response status is not 200" do
-            let(:response) do
-              double(:response, :status => 400, :body => body)
+            let(:status) do
+              400
             end
 
             let(:body) do
@@ -176,23 +160,18 @@ describe Piv::CLI do
               }
             end
 
-
             it "exits with a status of 1" do
               expect { run_command }.to exit_with_code(1)
             end
 
-            describe do
-              let(:stub_exit) { true }
-
-              it "prints the error msg" do
-                expect { run_command }.to output(/err msg/).to_stderr
-              end
+            it "prints the error msg" do
+              allow_exit!
+              expect { run_command }.to output(/err msg/).to_stderr
             end
 
           end
         end
       end
     end
-
   end
 end
