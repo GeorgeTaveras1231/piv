304ef66415da0540effa62c224b63212df413c6f
George Taveras <gtaveras@xogrp.com>
2014-12-25 19:36:59 -0500

    Made client receive connection on initialize and define a pattern for request methods

--- lib/piv/client.rb
+++ lib/piv/client.rb
@@ -2,28 +2,37 @@ module Piv
   class Client
     class NetworkError < StandardError; end
 
-    def initialize(url)
-      @connection = Faraday.new(url) do |c|
-        c.adapter :em_http
-        c.request :json
-        c.response :json
+    def initialize(connection, &config)
+      @connection = connection
+      @config = config || -> {}
+    end
+
+    def method_missing(meth, *args)
+      desired_method = "#{meth}_request"
+      if self.private_methods.include? desired_method.to_sym
+        begin
+          send(desired_method, *args)
+        rescue Faraday::TimeoutError, Faraday::ClientError => e
+          msg = case e
+                when Faraday::TimeoutError
+                  "the request timed out"
+                when Faraday::ClientError
+                  "the connection failed"
+                end
+
+          raise NetworkError, msg
+        end
+      else
+        super
       end
     end
 
-    def login(credentials)
+    private
+
+    def login_request(credentials)
       credentials.assert_valid_keys :user, :password
       @connection.basic_auth(credentials[:user], credentials[:password])
-      @connection.get(@connection.path_prefix + '/me')
-
-    rescue Faraday::TimeoutError, Faraday::ClientError => e
-      msg = case e
-            when Faraday::TimeoutError
-              "the request timed out"
-            when Faraday::ClientError
-              "the connection failed"
-            end
-
-      raise NetworkError, msg
+      @connection.get(@connection.path_prefix + '/me', &@config)
     end
   end
 end
