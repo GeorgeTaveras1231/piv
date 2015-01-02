fc61be3567d5f99e8de8176be0cf8c1e3c4cdd26
George Taveras <gtaveras@xogrp.com>
2015-01-02 14:18:02 -0500

    require JDBC driver for JRUBY

--- lib/piv.rb
+++ lib/piv.rb
@@ -1,39 +1,40 @@
 require 'thor'
+require 'jdbc/sqlite3' if defined?(JRUBY_VERSION)
 require 'active_record'
 require 'pry'
 require 'faraday'
 require 'faraday_middleware'
 require 'em-http-request'
-require "piv/version"
-require "piv/core_ext/kernel"
-require "piv/helpers/login"
-require "piv/helpers/logout"
-require "piv/helpers/whoami"
-require "piv/helpers/formatter"
-require "piv/helpers/formatter/parser"
-require "piv/helpers/application"
-require "piv/helpers/projects"
-require "piv/helpers/projects/list"
-require "piv/helpers/shell"
-require "piv/helpers/iterations"
+require 'piv/version'
+require 'piv/core_ext/kernel'
+require 'piv/helpers/login'
+require 'piv/helpers/logout'
+require 'piv/helpers/whoami'
+require 'piv/helpers/formatter'
+require 'piv/helpers/formatter/parser'
+require 'piv/helpers/application'
+require 'piv/helpers/projects'
+require 'piv/helpers/projects/list'
+require 'piv/helpers/shell'
+require 'piv/helpers/iterations'
 
-require "piv/subcommands/projects"
-require "piv/subcommands/stories"
+require 'piv/subcommands/projects'
+require 'piv/subcommands/stories'
 
-require "piv/event_handler"
-require "piv/thor_shell_help"
-require "piv/application"
-require "piv/runner"
+require 'piv/event_handler'
+require 'piv/thor_shell_help'
+require 'piv/application'
+require 'piv/runner'
 
-require "piv/client"
-require "piv/micro_command"
+require 'piv/client'
+require 'piv/micro_command'
 
 
-require "piv/models/concerns/currentable"
+require 'piv/models/concerns/currentable'
 
-require "piv/models/session"
-require "piv/models/project"
-require "piv/models/story"
+require 'piv/models/session'
+require 'piv/models/project'
+require 'piv/models/story'
 
 
 module Piv
