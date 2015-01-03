7ba62057b857540d3604ca620f9cc2f0086613c7
George Taveras <gtaveras@xogrp.com>
2015-01-03 17:26:38 -0500

    removed stale code

--- lib/piv/helpers/projects.rb
+++ lib/piv/helpers/projects.rb
@@ -29,16 +29,6 @@ module Piv
 
           end
 
-          # TODO: attempt to update projects rather than destroy and create
-          current_session.projects.destroy_all
-
-          project_objects = response.body.map do |project|
-            Project.new(:name => project['name'],
-                        :id => project['id'])
-          end
-
-          current_session.projects << project_objects
-
           event_handler.trigger :success, response
         else
           event_handler.trigger :failure, response
